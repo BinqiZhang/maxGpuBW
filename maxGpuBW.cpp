b1e7e85f3e3245139bb806ee6b073e3ec7e1e415
binqi.zhang <binqizhang1989@gmail.com>
2017-08-28 14:52:52 +0800

    correct the code style&&use the randome value instead

--- maxGpuBW.cpp
+++ maxGpuBW.cpp
@@ -29,27 +29,27 @@ void testGpuMaxBw() {
 	img_size = IMG_WIDTH*IMG_HEIGHT;
 
 	//create texture array with upspecified image contentes
-    glBindTexture(GL_TEXTURE_2D, texid[0]);
-    glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, IMG_WIDTH, IMG_HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+	glBindTexture(GL_TEXTURE_2D, texid[0]);
+	glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, IMG_WIDTH, IMG_HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
 
-    glBindTexture(GL_TEXTURE_2D, texid[1]);
-    glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, IMG_WIDTH, IMG_HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
-	
-	srand((unsigned)time(NULL));	
+	glBindTexture(GL_TEXTURE_2D, texid[1]);
+	glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, IMG_WIDTH, IMG_HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+
+	srand((unsigned)time(NULL));
 	for (int i = 0; i < img_size; i++)
-	{	
-		memset(tmp_buf0+i, 2*i, 1);
-		memset(tmp_buf1+i, 2*i+1, 1);
-	}  
-	
+	{
+		memset(tmp_buf0+i, (rand()%255), 1);
+		memset(tmp_buf1+i, (rand()%255), 1);
+	}
+
 	glBindTexture(GL_TEXTURE_2D, 0);
 	glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
@@ -65,85 +65,84 @@ void testGpuMaxBw() {
 	glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, IMG_WIDTH, IMG_HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, tmp_buf1);
 	free(tmp_buf1);
-   
-    glTexEnvx(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 
-    glEnable(GL_TEXTURE_2D);
-   
-    glDisable(GL_BLEND);
+	glTexEnvx(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 
-    glViewport(0, 0, IMG_WIDTH, IMG_HEIGHT);
+	glEnable(GL_TEXTURE_2D);
+	glDisable(GL_BLEND);
 
-    glFinish();
-    nsecs_t now = systemTime();
-    for (int i=0 ; i<loop ; i++) {
-        glBindFramebufferOES(GL_FRAMEBUFFER_OES, 1);
-        glFramebufferTexture2DOES(GL_FRAMEBUFFER_OES, GL_COLOR_ATTACHMENT0_OES, GL_TEXTURE_2D, texid[i%2], 0);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glColor4ub(i, 0, 0, 0);
-        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-    }
-    glFinish();
-    nsecs_t t = systemTime() - now;
-    printf("gpu max BW test: %dx%d:  time=%fms  BW=%f GB/s\n", IMG_WIDTH, IMG_HEIGHT, t/1000000.0f, img_size*4*2*loop/1024.0f/1024.0f/(t/1000000.0f));
-	
+	glViewport(0, 0, IMG_WIDTH, IMG_HEIGHT);
+
+	glFinish();
+	nsecs_t now = systemTime();
+	for (int i=0 ; i<loop ; i++) {
+		glBindFramebufferOES(GL_FRAMEBUFFER_OES, 1);
+		glFramebufferTexture2DOES(GL_FRAMEBUFFER_OES, GL_COLOR_ATTACHMENT0_OES, GL_TEXTURE_2D, texid[i%2], 0);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glColor4ub(i, 0, 0, 0);
+		glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+		}
+	glFinish();
+	nsecs_t t = systemTime() - now;
+	printf("gpu max BW test: %dx%d:  time=%fms  BW=%f GB/s\n", IMG_WIDTH, IMG_HEIGHT, t/1000000.0f, img_size*4*2*loop/1024.0f/1024.0f/(t/1000000.0f));
+
 }
-int main(int argc, char** argv)
-{
-    EGLDisplay dpy;
-    EGLint majorVersion;
-    EGLint minorVersion;
-    EGLConfig config;
-    EGLint numConfigs = -1;
-    EGLSurface surface;
-    EGLContext context;
-    EGLint w = 320, h = 480;
 
-    dpy = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-    eglInitialize(dpy, &majorVersion, &minorVersion);
+int main(int argc, char** argv) {
+	EGLDisplay dpy;
+	EGLint majorVersion;
+	EGLint minorVersion;
+	EGLConfig config;
+	EGLint numConfigs = -1;
+	EGLSurface surface;
+	EGLContext context;
+	EGLint w = 320, h = 480;
 
-    EGLint configAttribs[] = {
-        EGL_SURFACE_TYPE, EGL_PBUFFER_BIT|EGL_WINDOW_BIT,
-        EGL_RED_SIZE,      5,
-        EGL_GREEN_SIZE,    6,
-        EGL_BLUE_SIZE,     5,
-        EGL_NONE
-    };
-    eglChooseConfig(dpy, configAttribs, &config, 1, &numConfigs);
-    EGLint surfaceAttribs[] = { EGL_WIDTH, w, EGL_HEIGHT, h, EGL_NONE };
-    surface = eglCreatePbufferSurface(dpy, config, surfaceAttribs);
-    context = eglCreateContext(dpy, config, NULL, NULL);
-    eglMakeCurrent(dpy, surface, surface, context);
- 
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	dpy = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+	eglInitialize(dpy, &majorVersion, &minorVersion);
 
-    glDisable(GL_BLEND);
-    glDisable(GL_DITHER);
+	EGLint configAttribs[] = {
+	EGL_SURFACE_TYPE, EGL_PBUFFER_BIT|EGL_WINDOW_BIT,
+	EGL_RED_SIZE,      5,
+	EGL_GREEN_SIZE,    6,
+	EGL_BLUE_SIZE,     5,
+	EGL_NONE
+	};
+	eglChooseConfig(dpy, configAttribs, &config, 1, &numConfigs);
+	EGLint surfaceAttribs[] = { EGL_WIDTH, w, EGL_HEIGHT, h, EGL_NONE };
+	surface = eglCreatePbufferSurface(dpy, config, surfaceAttribs);
+	context = eglCreateContext(dpy, config, NULL, NULL);
+	eglMakeCurrent(dpy, surface, surface, context);
 
-    const GLfloat vertices[4][2] = {
-           { -1, -1 },
-           { -1,  1 },
-           {  1,  1 },
-           {  1, -1 }
-    };
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    const GLfloat texCoords[4][2] = {
-           { 0,  0 },
-           { 1,  0 },
-           { 1,  1 },
-           { 0,  1 }
-    };
+	glDisable(GL_BLEND);
+	glDisable(GL_DITHER);
 
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-    glVertexPointer(2, GL_FLOAT, 0, vertices);
-    glTexCoordPointer(2, GL_FLOAT, 0, texCoords);
+	const GLfloat vertices[4][2] = {
+	   { -1, -1 },
+	   { -1,  1 },
+	   {  1,  1 },
+	   {  1, -1 }
+	};
+
+	const GLfloat texCoords[4][2] = {
+	   { 0,  0 },
+	   { 1,  0 },
+	   { 1,  1 },
+	   { 0,  1 }
+	};
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+	glVertexPointer(2, GL_FLOAT, 0, vertices);
+	glTexCoordPointer(2, GL_FLOAT, 0, texCoords);
 
 	testGpuMaxBw();
-      
-    eglTerminate(dpy);
 
-    while(1);    
-    return 0;
+	eglTerminate(dpy);
+
+	while(1);
+	return 0;
 
 }
